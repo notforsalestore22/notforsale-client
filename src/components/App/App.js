f63ef6e06abb2cae5a114af1fcf71a58fdb30920
Matteo <maoce007@gmail.com>
2024-01-12 18:30:25 +0100

    wa link updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -46,7 +46,7 @@ function App() {
         </AuthProvider>
       </ProductsProvider>
       <Footer />
-      <a href="https://bit.ly/3OFMeSE">
+      <a href="http://bit.ly/3RThHRT">
         <div className="whatsapp-btn">
           <img src="/img/whatsapp.svg" alt="" className="img-res" />
         </div>
